f6dbf4b67c24273e47eeaaf83cc41d4dc4c485d0
vaibhavdeo21 <vaibhavpdeo0028@gmail.com>
2026-01-26 12:59:58 +0530

    Minor Change

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -14,7 +14,6 @@ const Doctors = () => {
   const navigate = useNavigate();
   const [doctors, setDoctors] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  const [selectedDoctor, setSelectedDoctor] = useState(null);
   const [filterEmergency, setFilterEmergency] = useState(false);
   const backendUrl = import.meta.env.VITE_API_URL;
 
