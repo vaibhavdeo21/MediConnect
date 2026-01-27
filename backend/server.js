1ea248170b43eec4fe6604457364d30d9c995194
Vaibhav Pratap Deo <vaibhavpdeo0028@gmail.com>
2026-01-27 22:19:43 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Telemedicine API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
